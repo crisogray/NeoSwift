7bab71bbffa8129f7a7167ab2b617cacc276a6bd
crisogray <ben.gray00@gmail.com>
2023-02-04 00:43:17 +0000

    Add  ByteStringStackItem

--- Package.swift
+++ Package.swift
@@ -20,12 +20,11 @@ let package = Package(
         .package(url: "https://github.com/SwiftyLab/DynamicCodableKit.git", from: "1.0.0"),
         .package(url: "https://github.com/leif-ibsen/SwiftECC", from: "3.4.1"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.6.0"),
-        .package(url: "https://github.com/dejanskledar/SerializedSwift.git", from: "0.2.0"),
         .package(url: "https://github.com/greymass/swift-scrypt.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "NeoSwift",
-                dependencies: ["BigInt", "CryptoSwift", "DynamicCodableKit", "SwiftECC", "SerializedSwift",
+                dependencies: ["BigInt", "CryptoSwift", "DynamicCodableKit", "SwiftECC",
                                .product(name: "Scrypt", package: "swift-scrypt")]),
         .testTarget(name: "NeoSwiftTests",
                     dependencies: ["NeoSwift", "BigInt", "SwiftECC"]),
---
+++ Sources/NeoSwift/protocol/core/stackitem/ByteStringStackitem.swift
@@ -0,0 +1,67 @@
+
+
+import Foundation
+import BigInt
+
+public struct ByteStringStackItem: StackItem, Hashable {
+    
+    public var type: StackItemType = .byteString
+    let value: Bytes
+    
+    public init(from decoder: Decoder) throws {
+        self.value = (try? Self.stringToType(decoder)) ?? []
+    }
+    
+    public var valueString: String {
+        return value.toHexString().cleanedHexPrefix
+    }
+    
+    init(_ value: Bytes) {
+        self.value = value
+    }
+    
+    init(_ hexValue: String) {
+        self.value = hexValue.bytesFromHex
+    }
+    
+    public func getValue() throws -> AnyHashable {
+        if value == [] {
+            throw "Cannot get stack item value because it is null"
+        }
+        return value
+    }
+    
+    public func getAddress() throws -> String {
+        return try Hash160(value.reversed()).toAddress()
+    }
+    
+    public func getBoolean() throws -> Bool {
+        return try getInteger() > 0
+    }
+    
+    public func getByteArray() throws -> Bytes {
+        if value == [] {
+            throw "Cannot get stack item value because it is null"
+        }
+        return value
+    }
+    
+    public func getInteger() throws -> Int {
+        if value == [] {
+            throw "Cannot get stack item value because it is null"
+        }
+        return BInt(magnitude: value.reversed()).asInt()!
+    }
+    
+    public func getString() throws -> String {
+        guard let string = String(bytes: value, encoding: .utf8) else {
+            throw "Cannot cast stack item \(string) to a string."
+        }
+        return string
+    }
+    
+    public func getHexString() throws -> String {
+        return value.toHexString().cleanedHexPrefix
+    }
+
+}
